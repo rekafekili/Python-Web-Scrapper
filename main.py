9fa18711895e3f5b8630140615e8ea74d931f942
SeongYun Cho <rekafekili@gmail.com>
2026-01-19 16:11:21 +0900

    #2.3 Recap

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-# String
 my_name = "SeongYun Cho"
+age = 31
+dead = False
 
-print(my_name)
-
-# Boolean
-isLive = True
-
-if (isLive):
-    print("Alive")
-else:
-    print("Dead")
+print("Hello my name is", my_name)
+print("and I'm", age, "years old")
+print("I AM", "DEAD" if dead else "ALIVE") # 3항 연산자
