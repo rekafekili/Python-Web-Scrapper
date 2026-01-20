f119af085734c74960c7bc6e80385a921a4cb0a8
SeongYun Cho <rekafekili@gmail.com>
2026-01-20 14:07:36 +0900

    #2.6 Parameters

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def say_hello():
-    print("hello how r u?")
+def say_hello(user_name):
+    print("Hello, how r u?")
+    print("My name is", user_name)
 
-def say_bye():
-    print("bye bye")
-
-def say_hello_bye():
-    say_hello()
-    say_bye()
-
-say_hello_bye()
+say_hello("SeongYun")
