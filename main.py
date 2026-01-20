b3e1e091f60ef7be96becfcb2f52669c33b01fc2
SeongYun Cho <rekafekili@gmail.com>
2026-01-20 14:21:36 +0900

    #2.7 Multiple Parameters

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def say_hello(user_name):
+def say_hello(user_name, user_age):
     print("Hello, how r u?")
     print("My name is", user_name)
+    print("I'm", user_age, "years-old")
 
-say_hello("SeongYun")
+say_hello("SeongYun", 12)
