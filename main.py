ce106776c3bd5809d22b538a510cc3e7bcd03139
SeongYun Cho <rekafekili@gmail.com>
2026-01-21 13:44:50 +0900

    #2.11 Return Recap

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-def tax_calc(money):
-    return money * 0.35
+# Variable in String
+my_name = "nico"
+my_age = 12
+my_color_eyes = "brown"
 
-def pay_tax(tax):
-    print("Thank you for paying:", tax)
+print(f"Hello I'm {my_name}, I have {my_age} years in the earth, {my_color_eyes} is my eye color")
 
-to_pay = tax_calc(15000000)
-pay_tax(to_pay)
+# Juice Maker
+def make_juice(fruit):
+    return f"{fruit}+🥤"
+
+def add_ice(juice):
+    return f"{juice}+🧊"
+
+def add_sugar(iced_juice):
+    return f"{iced_juice}+🍬"
+
+juice = make_juice("🍎")
+iced_juice = add_ice(juice)
+perfect_juice = add_sugar(iced_juice)
+
+print(perfect_juice)
