aae95149b4c20272809a46ccb631e16fb5e97506
SeongYun Cho <rekafekili@gmail.com>
2026-01-21 13:57:28 +0900

    #3.1 Else & Elif

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-if 10 > 5:
-    print("True")
+password_correct = True
+
+if password_correct:
+    print("Here is your money")
+else:
+    print("Wrong Password")
+
+winner = 9
+
+if winner > 10:
+    print("Winner is greater than 10")
+elif winner < 10:
+    print("Winner is less than 10")
+else:
+    print("Winner is 10")
