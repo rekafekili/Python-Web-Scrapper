eadfa6e7e4a4c2e46d69a61242338b57f498b1c3
SeongYun Cho <rekafekili@gmail.com>
2026-01-24 02:13:53 +0900

    #3.3 And & Or

--- main.py
+++ main.py
@@ -1,38 +1,13 @@
-# 👇🏻 YOUR CODE 👇🏻:
+age = int(input("How old are you? "))
 
-def get_yearly_revenue(monthly_revenue):
-    return monthly_revenue * 12
+print("user answer :", age)
+print("user input type :", type(age))
 
-def get_yearly_expenses(monthly_expenses):
-    return monthly_expenses * 12
-
-def get_tax_amount(profit):
-    if profit > 100000:
-        return profit * 0.25
-    else:
-        return profit * 0.15
-
-def apply_tax_credits(tax_amount, tax_credits):
-    return tax_amount * tax_credits
-
-
-# /YOUR CODE
-
-# BLUEPRINT | DONT EDIT
-
-monthly_revenue = 5500000
-monthly_expenses = 2700000
-tax_credits = 0.01
-
-yearly_revenue = get_yearly_revenue(monthly_revenue)
-yearly_expenses = get_yearly_expenses(monthly_expenses)
-
-profit = yearly_revenue - yearly_expenses
-
-tax_amount = get_tax_amount(profit)
-
-final_tax_amount = tax_amount - apply_tax_credits(tax_amount, tax_credits)
-
-print(f"Your tax bill is: ${final_tax_amount}")
-
-# /BLUEPRINT
+if age < 18:
+    print("You can't drink.") 
+elif age >= 18 and age <= 35:
+    print("You drink beer!")
+elif age == 60 or age == 70:
+    print("Birthday party!")
+else:
+    print("Go ahead!")
