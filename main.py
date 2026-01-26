2124ee23c13da2e0e63acaeec8a23ac65ebd0fde
SeongYun Cho <rekafekili@gmail.com>
2026-01-26 23:31:44 +0900

    #3.6 Python Casino

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-# Single-Line Comment
+from random import randint
 
-"""
-Multiple-Line 
-Comment
-"""
+print("Welcome to Python Casino")
+pc_choice = randint(1, 50)
 
-distance = 0
+playing = True
 
-while distance < 20:
-    print("I'm running:", distance, "km")
-    distance = distance + 1
+while playing:
+    user_choice = int(input("Choose number: "))
+    if user_choice == pc_choice:
+        print("You won!")
+        playing = False
+    elif user_choice > pc_choice:
+        print("Lower!")
+    elif user_choice < pc_choice:
+        print("Higher!")
