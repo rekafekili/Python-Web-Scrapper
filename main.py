b68bc45c9636484b31f85e2fadfc47aeffb57c9e
SeongYun Cho <rekafekili@gmail.com>
2026-01-26 23:25:37 +0900

    #3.5 While

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from random import randint
+# Single-Line Comment
 
-user_choice = int(input("Choose number:"))
-pc_choice = randint(10, 50)
+"""
+Multiple-Line 
+Comment
+"""
 
-if user_choice == pc_choice:
-    print("You won!")
-elif user_choice > pc_choice:
-    print("Lower! Computer chose", pc_choice)
-elif user_choice < pc_choice:
-    print("Higher! Computer chose", pc_choice)
+distance = 0
 
+while distance < 20:
+    print("I'm running:", distance, "km")
+    distance = distance + 1
