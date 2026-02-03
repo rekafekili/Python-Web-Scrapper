418aeac44b8443f33f0a4522e7cff2e457165b5a
SeongYun Cho <rekafekili@gmail.com>
2026-02-03 16:38:45 +0900

    #4.2 Tuples

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+daysTuple = ("Mon", "Tue", "Wed")
 
-print(days_of_week)
+# List와 다른 점 : Tuple은 변경이 불가능함. (Immutable Sequence)
 
-days_of_week.reverse()
-print(days_of_week)
-
-days_of_week.remove("Sun")
-print(days_of_week)
-
-print(days_of_week[0], days_of_week[2], days_of_week[4])
-
-days_of_week.clear()
-print(days_of_week)
+print(daysTuple[0])
+print(daysTuple[-1])
