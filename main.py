795ae63b674eeae89cd05dbdd3a437d6951a4380
SeongYun Cho <rekafekili@gmail.com>
2026-02-04 14:24:35 +0900

    #4.5 For Loops

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-# Dictionary
+# For Loops
+websites = ("google.com", "airbnb.com", "twitter.com", "facebook.com", "tiktok.com")
 
-player = {"name": "nico", "age": 12, "alive": True, "fav_food": ["Pizza", "Hamburger"]}
-
-print(player)
-print(player.keys())
-print(player.values())
-print(player.items())
-print(player.get("name"))
-print(player.get("fav_movie"))
-print(player["fav_food"])
-
-player.pop("age")
-print(player)
-
-player["age"] = 12
-print(player)
-
-player["fav_food"].append("Noodles")
-print(player)
+for website in websites:
+    print("Website:", website)
