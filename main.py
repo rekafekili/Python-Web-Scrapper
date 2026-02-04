94ce96af0c2fae07cc4f16f58b4033044c22ebe7
SeongYun Cho <rekafekili@gmail.com>
2026-02-04 17:26:54 +0900

    #5.1 Why We Need OOP

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
-from requests import get
+# person = {"name": "Nico", "xp": 1000, "team": "Team X"}
 
-# For Loops
-websites = (
-    "google.com",
-    "https://airbnb.com",
-    "twitter.com",
-    "https://facebook.com",
-    "https://tiktok.com",
-)
 
-results = {}
+def create_player(name, xp, team):
+    return {"name": name, "xp": xp, "team": team}
 
-for website in websites:
-    if not website.startswith("https://"):
-        website = f"https://{website}"
-    response = get(website)
-    if response.status_code == 200:
-        # print(f"{website} is OK")
-        results[website] = True
-    else:
-        # print(f"{website} is not OK")
-        results[website] = False
 
-print(results)
+def introduct_player(player):
+    name = player["name"]
+    team = player["team"]
+    print(f"Hello! My name is {name} and I play for {team}")
+
+
+person = create_player("nico", 1500, "Team Y")
+introduct_player(person)
