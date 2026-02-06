3c8b570747ce3294a764d195d63f96ac9e8e84aa
SeongYun Cho <rekafekili@gmail.com>
2026-02-06 23:43:41 +0900

    #5.5 Inheritance part Two

--- main.py
+++ main.py
@@ -4,16 +4,29 @@ class Dog:
         self.breed = breed
         self.age = age
 
+    def __str__(self):
+        return f"{self.breed} / {self.name} / {self.age}"
+
 
 class GuardDog(Dog):
+    def __init__(self, name, breed):
+        super().__init__(name, 5, breed)
+        self.aggresive = True
+
     def rrrr(self):
         print("Stay Away!")
 
 
 class Puppy(Dog):
+    def __init__(self, name, breed):
+        super().__init__(name, 2, breed)
+        self.spoiled = True
+
     def woof_woof(self):
         print("Woof Woof!")
 
 
 ruffus = Puppy(name="Ruffus", breed="Beagle")
-bibi = Puppy(name="Bibi", breed="Dalmatian")
+bibi = GuardDog(name="Bibi", breed="Dalmatian")
+
+print(ruffus, "//", bibi)
