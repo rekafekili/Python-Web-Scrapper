6bed9f7d38a76ca11a36ec31fa009e1b24ab863b
SeongYun Cho <rekafekili@gmail.com>
2026-02-07 00:04:24 +0900

    #5.7 Code Challenge

--- main.py
+++ main.py
@@ -1,32 +1,60 @@
-class Dog:
-    def __init__(self, name, age, breed):
+class Player:
+    def __init__(self, name, team):
         self.name = name
-        self.breed = breed
-        self.age = age
+        self.xp = 1500
+        self.team = team
 
-    def __str__(self):
-        return f"{self.breed} / {self.name} / {self.age}"
-
-
-class GuardDog(Dog):
-    def __init__(self, name, breed):
-        super().__init__(name, 5, breed)
-        self.aggresive = True
-
-    def rrrr(self):
-        print("Stay Away!")
+    def introduce(self):
+        print(f"My name is {self.name}. My team is {self.team}")
 
 
-class Puppy(Dog):
-    def __init__(self, name, breed):
-        super().__init__(name, 2, breed)
-        self.spoiled = True
+class Team:
+    def __init__(self, name):
+        self.name = name
+        self.players = []
 
-    def woof_woof(self):
-        print("Woof Woof!")
+    def add_player(self, name):
+        new_player = Player(name, self.name)
+        self.players.append(new_player)
+
+    def remove_player(self, name):
+        for player in self.players:
+            if player.name == name:
+                self.players.remove(player)
+                print(f"Player {player.name} is Removed")
+                break
+
+    def show_total_xp(self):
+        total_xp = 0
+        for player in self.players:
+            total_xp += player.xp
+        print(f"Our Team Total Xp is {total_xp}")
+
+    def show_players(self):
+        for player in self.players:
+            player.introduce()
 
 
-ruffus = Puppy(name="Ruffus", breed="Beagle")
-bibi = GuardDog(name="Bibi", breed="Dalmatian")
+nico = Player(name="nico", team="Team X")
+nico.introduce()
 
-print(ruffus, "//", bibi)
+lynn = Player(name="lynn", team="Team Y")
+lynn.introduce()
+
+team_x = Team(name="Team X")
+team_x.add_player(nico.name)
+
+team_y = Team(name="Team Y")
+team_y.add_player(lynn.name)
+
+team_x.show_players()
+team_y.show_players()
+
+team_x.show_total_xp()
+team_y.show_total_xp()
+
+team_x.remove_player(nico.name)
+team_x.show_players()
+
+team_y.remove_player(lynn.name)
+team_y.show_players()
