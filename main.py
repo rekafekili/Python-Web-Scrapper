e4b7fc60c7d3f238e4ece2876399eda87efc0eab
SeongYun Cho <rekafekili@gmail.com>
2026-02-11 00:40:04 +0900

    #6.2 BeautifulSoup

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# Scrapping Site
+import requests
+
+# import cloudscraper
+from bs4 import BeautifulSoup
+
+# scraper = cloudscraper.create_scraper()
+url = "https://weworkremotely.com/remote-full-time-jobs"
+
+# response = scraper.get(url)
+response = requests.get(url)
+
+# print(response.content)
+
+soup = BeautifulSoup(response.content, "html.parser")
+
+jobs = soup.find("section", class_="jobs").find_all("li")
+
+print(jobs)
