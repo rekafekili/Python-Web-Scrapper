f45db31422d39237da03a5728f7b5c61a81244eb
SeongYun Cho <rekafekili@gmail.com>
2026-02-12 23:09:11 +0900

    #6.4 Recap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+baseUrl = "https://weworkremotely.com"
 url = "https://weworkremotely.com/remote-full-time-jobs"
 
 response = requests.get(url)
@@ -9,13 +10,22 @@ soup = BeautifulSoup(response.content, "html.parser")
 
 jobTags = soup.find("section", class_="jobs").find_all("li")[:-1]
 
+all_jobs = []
+
 for jobTag in jobTags:
     title = jobTag.find("h3").string.strip()
     region = jobTag.find("p", class_="new-listing__company-headquarters").get_text(
         strip=True
     )
     company = jobTag.find("p", class_="new-listing__company-name").get_text(strip=True)
-    print(title)
-    print(region)
-    print(company)
-    print("================")
+    link = jobTag.find("a", class_="listing-link--unlocked")["href"]
+    all_jobs.append(
+        {
+            "title": title,
+            "region": region,
+            "company": company,
+            "link": f"{baseUrl}{link}",
+        }
+    )
+
+print(all_jobs)
