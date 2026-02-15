2634adf6abef071bf02fe3ede8be6f260ebdbeb1
SeongYun Cho <rekafekili@gmail.com>
2026-02-15 23:17:07 +0900

    #6.8 Playwright

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # Dynamic Scraping -> Playwright
+
+# 1. pip install playwright
+# 2. playwright install -> install browsers
+from playwright.sync_api import sync_playwright
+
+p = sync_playwright().start()
+
+browser = p.chromium.launch(headless=False)
+
+page = browser.new_page()
+
+page.goto("https://google.com")
+
+page.screenshot(path="screenshot.png")
