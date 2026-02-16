49ac839de74189740e5b82814978457b417f7d84
SeongYun Cho <rekafekili@gmail.com>
2026-02-16 23:18:18 +0900

    #6.9 Keyword Arguments

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-# Dynamic Scraping -> Playwright
+def plus(a, b):
+    return a + b
 
-# 1. pip install playwright
-# 2. playwright install -> install browsers
-from playwright.sync_api import sync_playwright
 
-p = sync_playwright().start()
+# Positional Argument
+plus(1, 2)
 
-browser = p.chromium.launch(headless=False)
-
-page = browser.new_page()
-
-page.goto("https://google.com")
-
-page.screenshot(path="screenshot.png")
+# Keyword Argument
+plus(b=1, a=2)
