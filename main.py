0531637ba91f06cc1bb78be622bcdbfaf2a4eef2
SeongYun Cho <rekafekili@gmail.com>
2026-02-21 00:14:12 +0900

    #6.13 Code Challenge - Add Input

--- main.py
+++ main.py
@@ -8,52 +8,58 @@ def sleep7():
     time.sleep(7)
 
 
-p = sync_playwright().start()
+def scrapeWanted(keyword: str):
+    p = sync_playwright().start()
 
-browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch(headless=False)
 
-page = browser.new_page()
+    page = browser.new_page()
 
-page.goto(
-    "https://www.wanted.co.kr/search?query=flutter&search_method=popular&tab=position"
-)
-
-content = page.content()
-
-p.stop()
-
-soup = BeautifulSoup(content, "html.parser")
-jobs_db: list[dict] = []
-
-jobs = soup.find_all("div", class_="JobCard_container__zQcZs")
-
-for job in jobs:
-    link = f"https://www.wanted.co.kr{job.find('a')['href']}"
-    title = job.find("strong", class_="JobCard_title___kfvj")
-    company_name = job.find(
-        "span",
-        class_="CompanyNameWithLocationPeriod_CompanyNameWithLocationPeriod__company__ByVLu wds-nkj4w6",
+    page.goto(
+        f"https://www.wanted.co.kr/search?query={keyword}&search_method=popular&tab=position"
     )
-    location = job.find(
-        "span",
-        class_="CompanyNameWithLocationPeriod_CompanyNameWithLocationPeriod__location__4_w0l wds-nkj4w6",
-    )
-    reward = job.find("span", class_="JobCard_reward__oCSIQ")
-    job = {
-        "title": title.string,
-        "company_name": company_name.string,
-        "location": location.string,
-        "reward": reward.string,
-        "link": link,
-    }
-    jobs_db.append(job)
 
-print(f"Jobs DB length:", len(jobs_db))
-print(jobs_db)
+    content = page.content()
 
-file = open("jobs.csv", mode="w")
-writer = csv.writer(file)
-writer.writerow(["title", "company", "location", "reward", "link"])
+    p.stop()
 
-for job in jobs_db:
-    writer.writerow(job.values())
+    soup = BeautifulSoup(content, "html.parser")
+    jobs_db: list[dict] = []
+
+    jobs = soup.find_all("div", class_="JobCard_container__zQcZs")
+
+    for job in jobs:
+        link = f"https://www.wanted.co.kr{job.find('a')['href']}"
+        title = job.find("strong", class_="JobCard_title___kfvj")
+        company_name = job.find(
+            "span",
+            class_="CompanyNameWithLocationPeriod_CompanyNameWithLocationPeriod__company__ByVLu wds-nkj4w6",
+        )
+        location = job.find(
+            "span",
+            class_="CompanyNameWithLocationPeriod_CompanyNameWithLocationPeriod__location__4_w0l wds-nkj4w6",
+        )
+        reward = job.find("span", class_="JobCard_reward__oCSIQ")
+        job = {
+            "title": title.string,
+            "company_name": company_name.string,
+            "location": location.string,
+            "reward": reward.string,
+            "link": link,
+        }
+        jobs_db.append(job)
+
+    print(f"Jobs DB length:", len(jobs_db))
+    print(jobs_db)
+
+    file = open(f"{keyword}_jobs.csv", mode="w")
+    writer = csv.writer(file)
+    writer.writerow(jobs_db[0].keys())
+
+    for job in jobs_db:
+        writer.writerow(job.values())
+    file.close()
+
+
+keywordInput = input("Enter a keyword: ")
+scrapeWanted(keywordInput)
