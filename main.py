94914a4f966fa5b690d21fc52a51240c9921ba33
SeongYun Cho <rekafekili@gmail.com>
2026-02-22 22:48:08 +0900

    #7.1 Hello Flask

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-from assignments.wanted_scrapper import WantedScrapper
+# from assignments.wanted_scrapper import WantedScrapper
 
-keywordInput = input("Enter a keyword: ")
-scrapper = WantedScrapper(keywordInput)
-scrapper.start()
+# keywordInput = input("Enter a keyword: ")
+# scrapper = WantedScrapper(keywordInput)
+# scrapper.start()
+
+from flask import Flask
+
+app = Flask("JobScrapper")
+
+
+# @ : decorator
+@app.route("/")
+def home():
+    return "hey there!"
+
+
+app.run()
